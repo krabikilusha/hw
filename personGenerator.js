976b2977dbd5f4d8323dfde950c535d295a3f108
krabikilusha <boss.ilia.2014@mail.ru>
2022-11-21 00:21:52 +0300

    New Progect

--- personGenerator.js
+++ personGenerator.js
@@ -35,6 +35,42 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Мария",
+            "id_3": "Ева",
+            "id_4": "Набива",
+            "id_5": "Анастасия",
+            "id_6": "Софья",
+            "id_7": "Елена",
+            "id_8": "Галина",
+            "id_9": "Дарья",
+            "id_10": "Виктория"
+        }
+    }`,
+    surnameFemaleJson: `{  
+        "count": 15,
+        "list": {
+            "id_1": "Иванова",
+            "id_2": "Смирнова",
+            "id_3": "Кузнецова",
+            "id_4": "Васильева",
+            "id_5": "Петрова",
+            "id_6": "Михайлова",
+            "id_7": "Новикова",
+            "id_8": "Федорова",
+            "id_9": "Кравцова",
+            "id_10": "Николаева",
+            "id_11": "Семёнова",
+            "id_12": "Славина",
+            "id_13": "Степанова",
+            "id_14": "Павлова",
+            "id_15": "Александрова",
+            "id_16": "Морозова"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -49,21 +85,23 @@ const personGenerator = {
 
     randomFirstName: function() {
 
-        return this.randomValue(this.firstNameMaleJson);
+        return this.randomValue(this.firstNameMaleJson)
+        if else this.randomValue(this.firstNameFemaleJson);
 
     },
 
 
      randomSurname: function() {
 
-        return this.randomValue(this.surnameJson);
+        return this.randomValue(this.surnameJson)
+        if else return this.randomValue(this.surnameFemaleJson);
 
     },
 
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender(GENDER_MALE, GENDER_FEMALE);
         this.person.firstName = this.randomFirstName();
         return this.person;
     }
